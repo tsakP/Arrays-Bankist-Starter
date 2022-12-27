f73bc80e1e0b8e09723f4691cdb871beb93ebf80
Petros Tsakonas <petros-tsakonas@hotmail.com>
2022-12-27 17:01:56 +0200

    practice array sorting, update of displaymovements function and implementation of btnSort click listener

--- script.js
+++ script.js
@@ -61,10 +61,12 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
-const displayMovements = function(movements) {
+const displayMovements = function(movements, sort = false) {
   containerMovements.innerHTML = '';
 
-  movements.forEach(function(mov, i) {
+  const movs = sort ? movements.slice().sort((a, b) => a - b) : movements;
+
+  movs.forEach(function(mov, i) {
     const type = mov > 0 ? 'deposit' : 'withdrawal';
     const html = `
       <div class="movements__row">
@@ -179,7 +181,7 @@ btnLoan.addEventListener('click', function(e) {
   updateUI(currentAccount);
   }
   inputLoanAmount.value = '';
-})
+});
 
 btnClose.addEventListener('click', function(e) {
   e.preventDefault();
@@ -198,7 +200,14 @@ btnClose.addEventListener('click', function(e) {
       labelWelcome.textContent = 'Log in to get started';
     }
   inputCloseUsername.value = inputClosePin.value = '';
-  })
+  });
+
+let sorted = false;   
+btnSort.addEventListener('click', function(e) {
+  e.preventDefault();
+  displayMovements(currentAccount.movements, !sorted);
+  sorted = !sorted;
+})
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // * Coding Challenge #1
@@ -316,6 +325,37 @@ const overallBalance2 = accounts
   .reduce((acc, mov) => acc + mov, 0);
 console.log(overallBalance);
 
+/////////////////////////////////////////////////
+
+/////////////////////////////////////////////////
+// Sorting Arrays
+
+/* // Strings
+const owners = ['Jonas', 'Zach', 'Adam', 'Martha'];
+console.log(owners.sort());
+console.log(owners); // sort method mutates the original array
+
+// Numbers
+console.log(movements);
+
+// return < 0, A, B (keep order)
+// return > 0, B, A (switch order)
+
+// Ascending Order
+// movements.sort((a,b) => {
+//   if (a > b) return 1;
+//   if (b > a) return -1;
+// });
+movements.sort((a,b) => a - b);
+console.log(movements);
+
+// Descending Order
+// movements.sort((a,b) => {
+//   if (a > b) return -1;
+//   if (b > a) return 1;
+// });
+movements.sort((a,b) => b - a);
+console.log(movements); */
 /////////////////////////////////////////////////
 
 /////////////////////////////////////////////////
